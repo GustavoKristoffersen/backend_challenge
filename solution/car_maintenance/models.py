d543cecc58b4b93643890fef8d7241187c9d1ffb
Gustavo William <gustavowilliam0980@gmail.com>
2021-09-04 13:46:01 -0300

    Create base models structure

--- solution/car_maintenance/models.py
+++ solution/car_maintenance/models.py
@@ -1,3 +1,12 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    gas_capacity = models.FloatField(default=100)
+    gas_count = models.FloatField(default=0)
+    
+class Tyre(models.Model):
+    id = models.AutoField(primary_key=True)
+    degradation = models.FloatField(default=0)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='tyres')
