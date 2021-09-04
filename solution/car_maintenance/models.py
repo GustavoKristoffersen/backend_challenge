7336cc268fce0ce96a8d89d49214508ec0c3ee74
Gustavo William <gustavowilliam0980@gmail.com>
2021-09-04 13:46:59 -0300

    Create method for adding a new tyre

--- solution/car_maintenance/models.py
+++ solution/car_maintenance/models.py
@@ -10,3 +10,17 @@ class Tyre(models.Model):
     id = models.AutoField(primary_key=True)
     degradation = models.FloatField(default=0)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='tyres')
+    
+    @classmethod
+    def createTyre(cls, car):
+        """
+        Creates a new tyre and adds it to an instance of car.
+
+        :param car: a car instance to which the tyre is going to be added.
+        :return: the instance of the tyre created.
+        :raises: ValidationError: if the car already has 4 tyres.
+        """
+
+        if car.tyres.count() < 4:
+            return cls.objects.create(car=car)
+        raise ValidationError(message='Car instance exceeded the maximum limit of tyres')
